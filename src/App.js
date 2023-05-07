3f83b8a2148b5fc84d8fd14799ecf1027ad9d12c
Corina <corina@danitt.com>
2023-05-07 10:31:01 +0200

    changed heading using mouseover and mouseout

--- src/App.js
+++ src/App.js
@@ -15,9 +15,36 @@ function App() {
     console.log("We clicked a thing!")
   }
 
+  /* 
+  {(event variable) => {
+    whatever block of code we wanna run when the event happens
+  }}
+  */
+
+  function mouseOverHeading(event) {
+    console.log("Element we moused over is \n" + event.target);
+    event.target.style.backgroundColor = "red";
+    console.log("Mouse over the heading");
+  }
+
+  function mouseOutOfHeading(event) {
+    console.log("Stopped mousing over the heading");
+    event.target.style.backgroundColor = null;
+  }
+
+  // onkeydown
+
+  // onblur & onfocus
+
+  // oncontextmenu
+
   return (
 
-    <div className="App" onClick={logWhenClicked} style={{ backgroundColor: "darkblue", width: "100vw", height: "100vh" }}>
+    <div className="App" onClick={logWhenClicked} style={{ backgroundColor: "lightgrey", width: "100vw", height: "100vh" }}>
+
+      <h1 onMouseOver={(event) => { mouseOverHeading(event) }}
+        onMouseOut={(event) => { mouseOutOfHeading(event) }}>Cool Website Name</h1>
+
       {someFruit.map((fruit) => {
         return <p key={fruit} onClick={(event) => fruitClickLogger(fruit, event)}> Super awesome {fruit}</p>
       })}
